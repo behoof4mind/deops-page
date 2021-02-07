bb884026c9050f98a9044a4c9d3d9a4af71dc5e4
Denis <denis.lavrushko@infobip.com>
2021-02-08 00:54:48 +0300

    Changed some colors Added TLS

--- devops-page.go
+++ devops-page.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"devops-page/bootstrap"
 	"devops-page/middleware/identity"
 	"devops-page/routes"
+	"golang.org/x/crypto/acme/autocert"
+	"net/http"
+	"strings"
 )
 
 func newApp() *bootstrap.Bootstrapper {
@@ -14,9 +18,24 @@ func newApp() *bootstrap.Bootstrapper {
 }
 
 func main() {
-	app := newApp()
-	app.Listen(":443")
-	//certmagic.DefaultACME.Agreed = true
-	//certmagic.DefaultACME.Email = "dlavrushko@protonmail.com"
-	//certmagic.HTTPS([]string{"dlavrushko.de"}, app)
+	//app := newApp()
+	//app.Listen(":443")
+	// LetsEncrypt setup
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("dlavrushko.de"), // your domain here
+		Cache:      autocert.DirCache("certs"),              // folder for storing certificates
+	}
+	server := &http.Server{
+		Addr:      ":8081",
+		TLSConfig: &tls.Config{GetCertificate: certManager.GetCertificate},
+	}
+	// open https server
+	_ = server.ListenAndServeTLS("", "")
+
+	// redirect everything to https
+	go http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqhost := strings.Split(r.Host, ":")[0]
+		http.Redirect(w, r, "https://"+reqhost+r.URL.Path, http.StatusMovedPermanently)
+	}))
 }
