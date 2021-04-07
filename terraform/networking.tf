6f4a35fa6ff32265e165b2c4b119936a63c1cb3c
Denis <denis.lavrushko@infobip.com>
2021-04-07 14:45:43 +0300

    added s3 and removed rds subnet-group

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "devops_page" {
-  cidr_block         = "172.16.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "devops-page-${var.env_prefix}"
