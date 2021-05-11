0a55e157fe0e848139fefe358a62da721ae44702
Denis <denis.lavrushko@infobip.com>
2021-05-11 15:28:13 +0300

    checking CI/CD

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -26,7 +26,12 @@ resource "aws_security_group" "https-web-access" {
     protocol    = "tcp"
     cidr_blocks = [aws_vpc.devops_page.cidr_block]
   }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.devops_page.cidr_block]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -91,6 +96,12 @@ resource "aws_security_group" "elb" {
   vpc_id = aws_vpc.devops_page.id
   name   = "devops-page-elb"
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 443
     to_port     = 443
